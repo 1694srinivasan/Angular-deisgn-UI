0934ad7220e2b8c858de024cd657d9f9e3a8a8b6
Srinivasan <srinivasan@niyatainfotech.com>
2017-04-25 17:09:54 +0530

    improvised ui

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,4 @@
-var app = angular.module('UIApp' , ['ngRoute' , 'ngMaterial'])
+var app = angular.module('UIApp' , ['ngRoute' , 'ngMaterial' , 'datatables'])
 app.config(function($mdIconProvider , $mdThemingProvider , $mdAriaProvider){
 		$mdThemingProvider.theme('default')
 		.primaryPalette('green')
@@ -12,6 +12,18 @@ app.config(function($routeProvider) {
         templateUrl : "views/dashboard.html",
         controller: 'dashboardctrl',
     })
+    .when("/uielements",{
+    	templateUrl : "views/uielements.html",
+    	controller : 'uictrl',
+    })
+    .when("/chart",{
+    	templateUrl : "views/chart.html",
+    	controller : 'chartctrl',
+    })
+    .when("/table",{
+    	templateUrl : "views/table.html",
+    	controller : 'tablectrl',
+    })
     .otherwise({
     	redirectTo : '/'
     });
@@ -20,10 +32,500 @@ app.config(function($routeProvider) {
 app.controller('dashboardctrl', function($scope, $http , $location ,$rootScope,$mdSidenav) {
 	$scope.toggleLeft = buildToggler('left');
     $scope.toggleRight = buildToggler('right');
-
     function buildToggler(componentId) {
       return function() {
         $mdSidenav(componentId).toggle();
       };
     }
+
+    $scope.ui = function(){
+		$location.path('/uielements');
+	}
+	$scope.dash = function(){
+		$location.path('/dashboard');
+	}
+	$scope.ui1 = function(){
+		$location.path('/uielements');
+	}
+	$scope.dash1 = function(){
+		$location.path('/dashboard');
+	}
+	$scope.pilot = function(){
+		$location.path('/dashboard');
+	}
+	$scope.chart = function(){
+		$location.path('/chart');
+	}
+	$scope.chart1 = function(){
+		$location.path('/chart');
+	}
+	$scope.table = function(){
+		$location.path('/table');
+	}
+	$scope.table1 = function(){
+		$location.path('/table');
+	}
+});
+
+app.controller('uictrl', function($scope, $http , $location ,$rootScope,$mdSidenav) {
+	$scope.toggleLeft = buildToggler('left');
+    $scope.toggleRight = buildToggler('right');
+    function buildToggler(componentId) {
+      return function() {
+        $mdSidenav(componentId).toggle();
+      };
+    }
+    $scope.ui = function(){
+		$location.path('/uielements');
+	}
+	$scope.dash = function(){
+		$location.path('/dashboard');
+	}
+	$scope.pilot = function(){
+		$location.path('/dashboard');
+	}
+	$scope.chart = function(){
+		$location.path('/chart');
+	}
+	$scope.table = function(){
+		$location.path('/table');
+	}
+});
+
+app.controller('chartctrl', function($scope, $http , $location ,$rootScope,$mdSidenav) {
+	$scope.toggleLeft = buildToggler('left');
+    $scope.toggleRight = buildToggler('right');
+    function buildToggler(componentId) {
+      return function() {
+        $mdSidenav(componentId).toggle();
+      };
+    }
+    $scope.ui = function(){
+		$location.path('/uielements');
+	}
+	$scope.dash = function(){
+		$location.path('/dashboard');
+	}
+	$scope.pilot = function(){
+		$location.path('/dashboard');
+	}
+	$scope.chart = function(){
+		$location.path('/chart');
+	}
+	$scope.table = function(){
+		$location.path('/table');
+	}
+
+	Highcharts.chart('container1', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Monthly Average Rainfall'
+    },
+    subtitle: {
+        text: 'Source: WorldClimate.com'
+    },
+    xAxis: {
+        categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+        ],
+        crosshair: true
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Rainfall (mm)'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: 'Tokyo',
+        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+
+    }, {
+        name: 'New York',
+        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+
+    }, {
+        name: 'London',
+        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+
+    }, {
+        name: 'Berlin',
+        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+
+    }]
+});
+
+	Highcharts.chart('container4', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Browser market shares January, 2015 to May, 2015'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Microsoft Internet Explorer',
+            y: 56.33
+        }, {
+            name: 'Chrome',
+            y: 24.03,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Firefox',
+            y: 10.38
+        }, {
+            name: 'Safari',
+            y: 4.77
+        }, {
+            name: 'Opera',
+            y: 0.91
+        }, {
+            name: 'Proprietary or Undetectable',
+            y: 0.2
+        }]
+    }]
+});
+
+	Highcharts.chart('container3', {
+    chart: {
+        type: 'area'
+    },
+    title: {
+        text: 'US and USSR nuclear stockpiles'
+    },
+    subtitle: {
+        text: 'Source: <a href="http://thebulletin.metapress.com/content/c4120650912x74k7/fulltext.pdf">' +
+            'thebulletin.metapress.com</a>'
+    },
+    xAxis: {
+        allowDecimals: false,
+        labels: {
+            formatter: function () {
+                return this.value; // clean, unformatted number for year
+            }
+        }
+    },
+    yAxis: {
+        title: {
+            text: 'Nuclear weapon states'
+        },
+        labels: {
+            formatter: function () {
+                return this.value / 1000 + 'k';
+            }
+        }
+    },
+    tooltip: {
+        pointFormat: '{series.name} produced <b>{point.y:,.0f}</b><br/>warheads in {point.x}'
+    },
+    plotOptions: {
+        area: {
+            pointStart: 1940,
+            marker: {
+                enabled: false,
+                symbol: 'circle',
+                radius: 2,
+                states: {
+                    hover: {
+                        enabled: true
+                    }
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'USA',
+        data: [null, null, null, null, null, 6, 11, 32, 110, 235, 369, 640,
+            1005, 1436, 2063, 3057, 4618, 6444, 9822, 15468, 20434, 24126,
+            27387, 29459, 31056, 31982, 32040, 31233, 29224, 27342, 26662,
+            26956, 27912, 28999, 28965, 27826, 25579, 25722, 24826, 24605,
+            24304, 23464, 23708, 24099, 24357, 24237, 24401, 24344, 23586,
+            22380, 21004, 17287, 14747, 13076, 12555, 12144, 11009, 10950,
+            10871, 10824, 10577, 10527, 10475, 10421, 10358, 10295, 10104]
+    }, {
+        name: 'USSR/Russia',
+        data: [null, null, null, null, null, null, null, null, null, null,
+            5, 25, 50, 120, 150, 200, 426, 660, 869, 1060, 1605, 2471, 3322,
+            4238, 5221, 6129, 7089, 8339, 9399, 10538, 11643, 13092, 14478,
+            15915, 17385, 19055, 21205, 23044, 25393, 27935, 30062, 32049,
+            33952, 35804, 37431, 39197, 45000, 43000, 41000, 39000, 37000,
+            35000, 33000, 31000, 29000, 27000, 25000, 24000, 23000, 22000,
+            21000, 20000, 19000, 18000, 18000, 17000, 16000]
+    }]
+});
+
+	function getNow() {
+    var now = new Date();
+
+    return {
+        hours: now.getHours() + now.getMinutes() / 60,
+        minutes: now.getMinutes() * 12 / 60 + now.getSeconds() * 12 / 3600,
+        seconds: now.getSeconds() * 12 / 60
+    };
+}
+
+/**
+ * Pad numbers
+ */
+function pad(number, length) {
+    // Create an array of the remaining length + 1 and join it with 0's
+    return new Array((length || 2) + 1 - String(number).length).join(0) + number;
+}
+
+var now = getNow();
+
+// Create the chart
+Highcharts.chart('container2', {
+
+    chart: {
+        type: 'gauge',
+        plotBackgroundColor: null,
+        plotBackgroundImage: null,
+        plotBorderWidth: 0,
+        plotShadow: false,
+        height: 200
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    title: {
+        text: 'The Highcharts clock'
+    },
+
+    pane: {
+        background: [{
+            // default background
+        }, {
+            // reflex for supported browsers
+            backgroundColor: Highcharts.svg ? {
+                radialGradient: {
+                    cx: 0.5,
+                    cy: -0.4,
+                    r: 1.9
+                },
+                stops: [
+                    [0.5, 'rgba(255, 255, 255, 0.2)'],
+                    [0.5, 'rgba(200, 200, 200, 0.2)']
+                ]
+            } : null
+        }]
+    },
+
+    yAxis: {
+        labels: {
+            distance: -20
+        },
+        min: 0,
+        max: 12,
+        lineWidth: 0,
+        showFirstLabel: false,
+
+        minorTickInterval: 'auto',
+        minorTickWidth: 1,
+        minorTickLength: 5,
+        minorTickPosition: 'inside',
+        minorGridLineWidth: 0,
+        minorTickColor: '#666',
+
+        tickInterval: 1,
+        tickWidth: 2,
+        tickPosition: 'inside',
+        tickLength: 10,
+        tickColor: '#666',
+        title: {
+            text: 'Powered by<br/>Highcharts',
+            style: {
+                color: '#BBB',
+                fontWeight: 'normal',
+                fontSize: '8px',
+                lineHeight: '10px'
+            },
+            y: 10
+        }
+    },
+
+    tooltip: {
+        formatter: function () {
+            return this.series.chart.tooltipText;
+        }
+    },
+
+    series: [{
+        data: [{
+            id: 'hour',
+            y: now.hours,
+            dial: {
+                radius: '60%',
+                baseWidth: 4,
+                baseLength: '95%',
+                rearLength: 0
+            }
+        }, {
+            id: 'minute',
+            y: now.minutes,
+            dial: {
+                baseLength: '95%',
+                rearLength: 0
+            }
+        }, {
+            id: 'second',
+            y: now.seconds,
+            dial: {
+                radius: '100%',
+                baseWidth: 1,
+                rearLength: '20%'
+            }
+        }],
+        animation: false,
+        dataLabels: {
+            enabled: false
+        }
+    }]
+},
+
+    // Move
+    function (chart) {
+        setInterval(function () {
+
+            now = getNow();
+
+            if (chart.axes) { // not destroyed
+                var hour = chart.get('hour'),
+                    minute = chart.get('minute'),
+                    second = chart.get('second'),
+                    // run animation unless we're wrapping around from 59 to 0
+                    animation = now.seconds === 0 ?
+                        false : {
+                            easing: 'easeOutBounce'
+                        };
+
+                // Cache the tooltip text
+                chart.tooltipText =
+                    pad(Math.floor(now.hours), 2) + ':' +
+                    pad(Math.floor(now.minutes * 5), 2) + ':' +
+                    pad(now.seconds * 5, 2);
+
+
+                hour.update(now.hours, true, animation);
+                minute.update(now.minutes, true, animation);
+                second.update(now.seconds, true, animation);
+            }
+
+        }, 1000);
+
+    });
+
+/**
+ * Easing function from https://github.com/danro/easing-js/blob/master/easing.js
+ */
+Math.easeOutBounce = function (pos) {
+    if ((pos) < (1 / 2.75)) {
+        return (7.5625 * pos * pos);
+    }
+    if (pos < (2 / 2.75)) {
+        return (7.5625 * (pos -= (1.5 / 2.75)) * pos + 0.75);
+    }
+    if (pos < (2.5 / 2.75)) {
+        return (7.5625 * (pos -= (2.25 / 2.75)) * pos + 0.9375);
+    }
+    return (7.5625 * (pos -= (2.625 / 2.75)) * pos + 0.984375);
+};
+});
+
+app.controller('tablectrl', function($scope, $http , $location ,$rootScope,$mdSidenav) {
+	$scope.toggleLeft = buildToggler('left');
+    $scope.toggleRight = buildToggler('right');
+    function buildToggler(componentId) {
+      return function() {
+        $mdSidenav(componentId).toggle();
+      };
+    }
+    $scope.ui = function(){
+		$location.path('/uielements');
+	}
+	$scope.dash = function(){
+		$location.path('/dashboard');
+	}
+	$scope.pilot = function(){
+		$location.path('/dashboard');
+	}
+	$scope.chart = function(){
+		$location.path('/chart');
+	}
+	$scope.table = function(){
+		$location.path('/table');
+	}
+
+	$http({method: 'GET',url: 'https://angapi.herokuapp.com/temprature/'}).
+    then(function successCallback(data) {
+        $scope.temp_data = []
+        angular.forEach(data.data,function(data){
+            $scope.temp_data.push(data);
+        })
+    }, function errorCallback(data) {
+        $scope.error = data.data;
+        
+    });
+
+    $http({method: 'GET',url: 'https://angapi.herokuapp.com/product/'}).
+    then(function successCallback(data) {
+        $scope.userdata = []
+        angular.forEach(data.data,function(data){
+            $scope.userdata.push(data);
+        })
+    }, function errorCallback(data) {
+        $scope.error = data;
+        
+    });
 });
